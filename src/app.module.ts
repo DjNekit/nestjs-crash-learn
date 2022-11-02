075417efef85c689ee56cef482a08672e60a1418
Nikita Lipin <nikitalipin97@gmail.com>
2022-11-02 18:12:09 +0300

    logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
+import { logger } from './logger.middleware';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .forRoutes(UsersController);
+  }
+}
