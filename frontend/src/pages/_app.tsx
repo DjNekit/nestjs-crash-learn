1258b867ee1308a10ce7408639831fcea75f4cc6
Nikita Lipin <nikitalipin97@gmail.com>
2022-11-11 16:59:07 +0300

    per page layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,23 @@
+import { ReactElement, ReactNode } from 'react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { NextPage } from 'next'
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const defaultPage = (page: ReactElement) => page
+  const getLayout = Component.getLayout ?? defaultPage;
+  
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   )
 }
